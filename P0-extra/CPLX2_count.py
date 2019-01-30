933b76e1da402dbbbc3b33ed373baf98da98d385
RealARamirez <Antoniomanuelramirezmarquez@gmail.com>
2019-01-30 18:12:15 +0100

    Extra practices for the class.

--- P0-extra/CPLX2_count.py
+++ P0-extra/CPLX2_count.py
@@ -1,17 +1,16 @@
-from typing import List, T
+from typing import List
 NumA = 0
 NumC = 0
 NumG = 0
 NumT = 0
-with open("CPLX2.txt", "r") as f:
-    AuxVar = str(f)
-    AuxVar: List[str] = AuxVar.splitlines()
-    for elem in AuxVar:
-        if elem[0] != ">":
-            NumA = NumA + int(elem.count('A'))
-            NumC = NumC + int(elem.count('C'))
-            NumG = NumG + int(elem.count('G'))
-            NumT = NumT + int(elem.count('T'))
+AuxVar = open("CPLX2.txt", "r")
+AuxVar: List[str] = AuxVar.splitlines()
+for elem in AuxVar:
+    if elem[0] != ">":
+        NumA = NumA + int(elem.count('A'))
+        NumC = NumC + int(elem.count('C'))
+        NumG = NumG + int(elem.count('G'))
+        NumT = NumT + int(elem.count('T'))
 print('A:', NumA)
 print('C:', NumC)
 print('T:', NumT)
