147a37c236e291828a61354e7b2c4a24758b7b92
RealARamirez <Antoniomanuelramirezmarquez@gmail.com>
2019-02-14 10:49:52 +0100

    Extra practices for the class. Now working successfully.

--- P0-extra/CPLX2_count.py
+++ P0-extra/CPLX2_count.py
@@ -1,17 +1,21 @@
-from typing import List
 NumA = 0
 NumC = 0
 NumG = 0
 NumT = 0
-AuxVar = open("CPLX2.txt", "r")
-AuxVar: List[str] = AuxVar.splitlines()
+File = open("CPLX2.txt", "r")
+AuxVarRead = File.read()
+AuxVar = AuxVarRead.split("\n")
 for elem in AuxVar:
-    if elem[0] != ">":
-        NumA = NumA + int(elem.count('A'))
-        NumC = NumC + int(elem.count('C'))
-        NumG = NumG + int(elem.count('G'))
-        NumT = NumT + int(elem.count('T'))
+    try:
+        if elem[0] != ">":
+            NumA = NumA + int(elem.count('A'))
+            NumC = NumC + int(elem.count('C'))
+            NumG = NumG + int(elem.count('G'))
+            NumT = NumT + int(elem.count('T'))
+    except IndexError:
+        continue
 print('A:', NumA)
 print('C:', NumC)
 print('T:', NumT)
 print('G:', NumG)
+File.close()
