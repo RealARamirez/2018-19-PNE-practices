be755c8a3ff2cfd57bdf9546661d57b9091a2ce2
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-02-20 17:47:45 +0100

    Practice three.

--- P3/server_proccessor.py
+++ P3/server_proccessor.py
@@ -1,4 +1,32 @@
 from Seq import Seq
 
+# Create a function that allow us to solve what our server is expected to do
+# First argument must be an object of type Seq that is given by the user
+# Second argument is the command that are given by the user and describe the different operations
+def command_proccessor(seq, comm):
+    seq = Seq(seq)
+    if comm == "len":
+        return seq.len()
+    elif comm == "complement":
+        return seq.complement()
+    elif comm == "reverse":
+        return seq.complement()
+    elif comm == "countA":
+        return seq.count("A")
+    elif comm == "countT":
+        return seq.count("T")
+    elif comm == "countG":
+        return seq.count("G")
+    elif comm == "countC":
+        return seq.count("C")
+    elif comm == "percA":
+        return seq.perc("A")
+    elif comm == "percT":
+        return seq.perc("T")
+    elif comm == "percG":
+        return seq.perc("G")
+    elif comm == "percC":
+        return seq.perc("C")
+
 
 
