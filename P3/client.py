f4818e255ef0969181c820b3ed2bf4ea4bd2e714
RealARamirez <Antoniomanuelramirezmarquez@gmail.com>
2019-02-21 18:53:44 +0100

    Extra practices for the class.

--- P3/client.py
+++ P3/client.py
@@ -11,14 +11,17 @@ while True:
     print("Welcome to the program for working with sequences.\nType an empty input for knowing if the server is alive.\nOtherwise, type firstly a sequence, intro and separate with intro the propierties of the sequence, with an empty input you will send the message.")
 
     # Create a message for sending to the client
-    message = "\n"
+    message = []
 
     # Create a loop that add the successive inputs
     while True:
         inp = input("> ")
-        message.join(inp)
+        message.append(inp)
         if inp == "":
             break
+
+    # Transform the message into a string
+    message = "\n".join(message)
 
     # Create the socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
