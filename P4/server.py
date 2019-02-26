388d109a0ed0700ea3f940395abb1bb1c3ec626a
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-02-26 16:55:30 +0100

    Practice four.

---
+++ P4/server.py
@@ -0,0 +1,80 @@
+import socket
+import termcolor
+
+# Change this IP to yours!!!!!
+IP = "212.128.253.95"
+PORT = 8089
+MAX_OPEN_REQUESTS = 5
+
+def process_client(cs):
+    """Process the client request.
+    Parameters:  cs: socket for communicating with the client"""
+
+    # Read client message. Decode it as a string
+    msg = cs.recv(2048).decode("utf-8")
+
+    # Print the received message, for debugging
+    print()
+    print("Request message: ")
+    termcolor.cprint(msg, 'green')
+
+    variable = msg.splitlines()
+    var= variable[0].lstrip("GET ").rstrip(" HTTP/1.1")
+    print(var)
+    if var == "":
+        file = open("index.html", "r")
+        content = file.read()
+        file.close()
+    elif var == "/blue":
+        file = open("blue.html", "r")
+        content = file.read()
+        file.close()
+    elif var == "/pink":
+        file = open("pink.html", "r")
+        content = file.read()
+        file.close()
+    else:
+        file = open("error.html", "r")
+        content = file.read()
+        file.close()
+
+
+    statusline = "HTTP/1.1 200 OK\r\n"
+
+
+    header = "Content-Type: text/html\r\n"
+
+    header += "Content-lenth: {}\r\n".format(len(str.encode(content)))
+
+    response_msg = statusline + header + "\n" + content
+    cs.send(str.encode(response_msg))
+
+    # Close the socket
+    cs.close()
+
+
+# MAIN PROGRAM
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP and PORT
+serversocket.bind((IP, PORT))
+
+# Configure the server sockets
+# MAX_OPEN_REQUESTS connect requests before refusing outside connections
+serversocket.listen(MAX_OPEN_REQUESTS)
+
+print("Socket ready: {}".format(serversocket))
+
+while True:
+    # accept connections from outside
+    # The server is waiting for connections
+    print("Waiting for connections at {}, {} ".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    # Connection received. A new socket is returned for communicating with the client
+    print("Attending connections from client: {}".format(address))
+
+    # Service the client
+    process_client(clientsocket)
