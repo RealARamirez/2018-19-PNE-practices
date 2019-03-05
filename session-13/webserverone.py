046d6c52360b6edccd9fe7559c80dfbbd99621a8
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-03-05 16:24:51 +0100

    Session thirteen.

--- session-13/webserverone.py
+++ session-13/webserverone.py
@@ -4,24 +4,22 @@ import socketserver
 # Define the Server's port
 PORT = 8002
 
-# -- Create the handler
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         print("GET received")
-        print("Request line:" + self.requestline)
-        print(" Cmd: " + self.command)
-        print(" Path: " + self.path)
+        print("Request line" + self.requestline)
+        print("       Cmd:  " + self.command)
+        print("      Path:  " + self.path)
 
-        content = "Happy server!"
-
+        content = "I am the happy server! <3"
         self.send_response(200);
-        self.send_header("Content-Type", "text/plain")
-        self.send_header("Content-Length", len(str.encode(content))
-
+        self.send_header('Content-Type','text/plain')
+        self.send_header('Content-Length', len(str.encode(content)))
         self.end_headers()
 
-        self.wfile.write(str.encode("index.html"))
+        self.wfile.write(str.encode(content))
+        return
 
 
 Handler = TestHandler
