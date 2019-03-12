97c0be313718f98f779277116d04d4b41e8f3621
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-03-12 15:50:38 +0100

    Session fifteen.

--- session-15/first_server.py
+++ session-15/first_server.py
@@ -1,22 +1,24 @@
 import http.server
 import socketserver
+import termcolor
 
 # Define the port
 PORT = 8000
 
+
 class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         print("GET received")
-        print("Request line" + self.requestline)
+        termcolor.cprint(self.requestline, "green")
         print("       Cmd:  " + self.command)
         print("      Path:  " + self.path)
-        file = open("index.html", "r")
+        file = open("HTMLexample.html", "r")
         content = file.read()
         file.close()
-        path = str(self.path)
-        message = path.lstrip("/act?msg=")
+        path = self.path
+        message = path.lstrip("/echo?msg=")
         print(message)
-        self.send_response(200);
+        self.send_response(200)
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(content)))
         self.end_headers()
@@ -24,12 +26,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(str.encode(content))
         return
 
+
 Handler = TestHandler
 
+
 with socketserver.TCPServer(("", PORT), Handler) as Svr:
-    print("Serving at Port: ", PORT)
+    print("Serving at Port: {}".format(PORT))
     try:
         Svr.serve_forever()
     except KeyboardInterrupt:
         print("Program stopped")
         Svr.server_close()
+
+
+print("The server is stopped")
